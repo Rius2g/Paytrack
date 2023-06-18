8351e37f173d9d7bd33ae71ea24a91f2f0122732
Rius2g <marius.mollerhansen@hotmail.com>
2023-06-18 18:09:20 +0200

    good start on the rules

--- backend/Controllers/PayController.cs
+++ backend/Controllers/PayController.cs
@@ -30,7 +30,7 @@ public class PayController : BaseController
         return 0;
     }
     
-        
+    //get all the rules to pass in so we dont have to qeury the database for each shift
     var rules = connection.Query<Rules>(@"
         SELECT * FROM Rules WHERE UiD = @userID;",
         new { userID }).ToArray();
@@ -39,7 +39,9 @@ public class PayController : BaseController
     foreach(var shift in shifts_with_jobs)
     {
         ExpectedPay += CalculateBasePayHours(shift, shift.payRate);
-        ExpectedPay += CalculateExtraFromRules(rules, shift, shift.payRate);
+        if (rules != null)
+            ExpectedPay += CalculateExtraFromRules(rules, shift, shift.payRate);
+
     }
 
     // Access the data in the shifts_with_jobs and rules variables as needed
@@ -50,6 +52,25 @@ public class PayController : BaseController
     public double CalculateExtraFromRules(Rules[] rules, Shift shift, int basePay) //call this with each individual shift to find the extra pay
     {
         //case switch for each rule type and then call the appropriate method
+        foreach(var rule in rules)
+        {
+            if (rule.JobID == shift.jobbID)
+            {
+                switch(rule.RuleType)
+                {
+                    case "Day":
+                        return CalculateDayRule(rule, shift, basePay);
+                    case "Date":
+                        return CalculateDateRule(rule, shift, basePay);
+                    case "Time":
+                        return CalculateStartRule(rule, shift, basePay);
+                    case "Time and Day":
+                        return CalculateDayAndTimeRule(rule, shift, basePay);
+                    default:
+                        return 0;
+                }
+            }
+        }
         return 0;
     }
 
@@ -72,6 +93,66 @@ public class PayController : BaseController
             hours_worked += minutes_worked / 60;
         }
         return basepay * hours_worked;
+    }
+
+    public double CalculateDayRule(Rules rule, Shift shift, int basePay)
+    {
+        string dateString = shift.shiftDate.ToString();
+        int year = int.Parse(dateString.Substring(0, 4));
+        // Accessing the month
+        int month = int.Parse(dateString.Substring(4, 2));
+        // Accessing the date
+        int day = int.Parse(dateString.Substring(6, 2));
+
+        DateTime date = new DateTime(year, month, day);
+
+        if(rule.Day == date.DayOfWeek.ToString())
+        {
+         //check rate type
+            if(rule.RateType == "%")
+            {
+                return CalculateBasePayHours(shift, basePay * ((rule.Rate / 100) + 1)) - CalculateBasePayHours(shift, basePay);
+            }
+            else
+            {
+                return CalculateBasePayHours(shift, basePay+rule.Rate) - CalculateBasePayHours(shift, basePay);
+            }
+        }
+        return 0;
+    }
+
+    public double CalculateDateRule(Rules rule, Shift shift, int basePay)
+    {
+        //calculate the extra pay for a shift based on the date rule
+
+        //create 2 dateTime objects to check the date
+        // if(rule.Date == shift.shiftStartTime.Date)
+        // {
+        //     return basePay * rule.Rate;
+        // }
+        return 0;
+    }
+
+    public double CalculateStartRule(Rules rule, Shift shift, int basePay)
+    {
+        //calculate the extra pay for a shift based on the start rule
+        if(rule.Start == shift.shiftStartTime)
+        {
+            return basePay * rule.Rate;
+        }
+        return 0;
+    }
+
+    public double CalculateDayAndTimeRule(Rules rule, Shift shift, int basePay)
+    {
+        //calculate the extra pay for a shift based on the day and time rule
+        //create dateTime object with the string to check the day
+
+        // if(rule.Day == shift.shiftStartTime.DayOfWeek.ToString() && rule.Start == shift.shiftStartTime)
+        // {
+        //     return basePay * rule.Rate;
+        // }
+        return 0;
     }
     
 }
