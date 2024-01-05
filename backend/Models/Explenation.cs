968648f82e398495eae51045121bcbbe557b8d3e
Rius2g <marius.mollerhansen@hotmail.com>
2024-01-05 15:10:06 +0100

    created a module for pay explenation

---
+++ backend/Models/Explenation.cs
@@ -0,0 +1,13 @@
+namespace backend.Models
+{
+    public class Expleantion
+    {
+        public string? Job { get; set; } //job name
+        public int shifts { get; set; } //amount of shifts
+        public int Hours { get; set; } //hours worked that job in that range 
+        public int Rate { get; set; } //the rate of the job 
+        public int Salary { get; set; } //total compensation for that job in that range (base OR Rule)
+        public string? Type { get; set; } //base or rule addition
+        public string? RuleType { get; set; } //rule name
+    }
+}
